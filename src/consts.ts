6a96983cda817cf58c4e684bf89acbdd5ad73b64
Richard Jones <richard.jones@playtimestudios.com>
2026-04-19 16:04:55 +0100

    Revert "Reboot in Next.js"
    This reverts commit 21b6bdd705a807eb6549320262d4cdebfbd1e6e1.

---
+++ src/consts.ts
@@ -0,0 +1,71 @@
+import type { IconMap, SocialLink, Site } from '@/types'
+
+export const FEATURE_TOGGLE: Record<string, boolean> = {
+  authors: false
+}
+
+export const SITE: Site = {
+  title: 'Nomas Prime',
+  description:
+    'astro-erudite is a opinionated, unstyled blogging template—built with Astro, Tailwind, and shadcn/ui.',
+  href: 'https://astro-erudite.vercel.app',
+  author: 'nomasprime',
+  locale: 'en-US',
+  featuredPostCount: 2,
+  postsPerPage: 3,
+}
+
+export const NAV_LINKS: SocialLink[] = [
+  {
+    href: '/blog',
+    label: 'blog',
+  },
+  {
+    href: '/about',
+    label: 'about',
+  },
+]
+
+if (FEATURE_TOGGLE.authors) {
+  NAV_LINKS.push({
+    href: '/authors',
+    label: 'authors',
+  })
+}
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    href: 'https://www.youtube.com/@nomasprime',
+    label: 'YouTube',
+  },
+  {
+    href: 'https://bsky.app/profile/nomasprime.bsky.social',
+    label: 'BlueSky',
+  },
+  {
+    href: 'https://mastodon.social/@nomasprime',
+    label: 'Mastodon',
+  },
+  {
+    href: 'https://www.linkedin.com/in/nomasprime/',
+    label: 'LinkedIn',
+  },
+  {
+    href: 'https://github.com/nomasprime',
+    label: 'GitHub',
+  },
+  {
+    href: '/rss.xml',
+    label: 'RSS',
+  },
+]
+
+export const ICON_MAP: IconMap = {
+  Website: 'lucide:globe',
+  GitHub: 'ri:github-line',
+  LinkedIn: 'ri:linkedin-line',
+  BlueSky: 'ri:bluesky-line',
+  Mastodon: 'ri:mastodon-line',
+  YouTube: 'ri:youtube-line',
+  RSS: 'ri:rss-line',
+}
