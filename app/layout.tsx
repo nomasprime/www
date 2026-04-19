1723d6dbf2e02a98eb645275b5564d0604e6f57a
Richard Jones <richard.jones@playtimestudios.com>
2026-04-19 16:04:55 +0100

    Revert "Integrate PostHog client-side"
    This reverts commit 237a072c7b229419dfd9d8699d8ff4303e45a963.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Providers } from './providers';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>
-          {children}
-        </Providers>
+        {children}
+        <SpeedInsights />
       </body>
     </html>
   );
